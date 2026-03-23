4daf931bb1382a0e828f024d10b28ca52d808680
haithamkhaled2 <130885587+haithamkhaled2@users.noreply.github.com>
2026-03-23 13:17:16 +0200

    Add files via upload

---
+++ Occupatation Analysis/SQL/Occupation_Report.sql
@@ -0,0 +1,14 @@
+USE [FirstDB]
+GO
+
+/****** Object:  View [dbo].[Occupation_report]    Script Date: 3/23/2026 12:18:03 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE OR ALTER View [dbo].[Occupation_report] AS 
+SELECT * FROM Occupation ;
+GO
+
